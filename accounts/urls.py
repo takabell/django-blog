7d6fa9fa649647a93e58a4b371b442f29b9d8a7d
takabell <takato.suzuki@timeless-education.com>
2022-09-30 15:14:23 +0900

    パスワード変更機能を追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
 	#'prfile'はURLリンクのパスを書いている。
 	path('profile/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
 	path('profile/update/<int:pk>', views.UserUpdate.as_view(), name='user_update'),
+	#PasswordChangeViewとPasswordChangeDoneViewはDjangoのアカウント機能にあるやつ
+	path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+	path('password_change/done/', views.PasswordChangeDoneView.as_view(), name=('password_change_done')),
 ]
